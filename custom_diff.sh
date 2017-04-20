ee04b5863d6f343075ae9abd13ada523be0834c5
Yavor Mihaylov <yavor.st.m@gmail.com>
2017-04-20 21:20:16 +0300

    Add custom diff script

---
+++ custom_diff.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE=$1
+
+git diff $FILE
+
+if [[ $FILE = ""  ]]; then
+    exit
+fi
+
+echo 'What would you want me to do with this file?'
+echo '1) git add'
+echo '2) git checkout'
+echo '3) or 0) NO ACTION'
+echo '-------------------'
+echo -n 'Enter your choice: '
+
+read ACTION
+
+case $ACTION in
+"1")
+    git add $FILE
+    echo "$FILE is staged succesfully!"
+    ;;
+"2")
+    git checkout $FILE
+    echo "$FILE is discarded succesfully!"
+    ;;
+esac
